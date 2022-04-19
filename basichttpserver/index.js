67445b9191594076214a00873dce81237b0e19bf
sa1123 <sahil2398@yahoo.com>
2022-04-20 01:59:27 +0530

    read file

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,10 +1,19 @@
 const http = require("http");
 const port = 8000;
+const fs = require('fs');
 
 function requestHandler(req, res){
     console.log(req.url);
     res.writeHead(200, {'content-type': 'text/html'})
-    res.end('<h1>Gotcha!</h1>');
+    // res.end('<h1>Gotcha!</h1>')
+    fs.readFile('./index.html', function(err, data){
+        if(err){
+            console.log('error', err);
+            return res.end('<h1>Error</h1>');
+        }
+
+        return res.end(data);
+    });
 }
 
 const server = http.createServer(requestHandler);
