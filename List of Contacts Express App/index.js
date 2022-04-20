2ffc73ee9828b7c90176d1afc63274efd6217816
sa1123 <sahil2398@yahoo.com>
2022-04-21 00:50:16 +0530

    ejs template setup

--- List of Contacts Express App/index.js
+++ List of Contacts Express App/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const path = require('path');
 const port = 8000;
 
 const app = express();
 
+app.set('view engine', 'ejs');
+// app.set('views', )
+
+app.get('/', function(req, res){
+    // console.log(__dirname)
+    // res.send("<h1>Running</h1>");
+
+    return res.render('home');
+});
 
 app.listen(port, function(err){
     if (err) { console.log('Error in running the server', err);}
