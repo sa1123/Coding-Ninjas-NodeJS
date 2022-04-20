2823d2754c36d82e4a2c7010fdd1580358974ae4
sa1123 <sahil2398@yahoo.com>
2022-04-20 23:29:01 +0530

    multiple pages

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -6,14 +6,35 @@ function requestHandler(req, res){
     console.log(req.url);
     res.writeHead(200, {'content-type': 'text/html'})
     // res.end('<h1>Gotcha!</h1>')
-    fs.readFile('./index.html', function(err, data){
-        if(err){
+    // fs.readFile('./index.html', function(err, data){
+    //     if(err){
+    //         console.log('error', err);
+    //         return res.end('<h1>Error</h1>');
+    //     }
+
+    //     return res.end(data);
+    // });
+
+    let filePath;
+
+    switch(req.url){
+        case '/':
+            filePath = './index.html'
+            break;
+        case '/profile':
+            filePath = './profile.html'
+            break;
+        
+        default:
+            filePath = './404.html'
+    }
+
+    fs.readFile(filePath, function(err, data){
+        if (err){
             console.log('error', err);
             return res.end('<h1>Error</h1>');
         }
-
-        return res.end(data);
-    });
+    })
 }
 
 const server = http.createServer(requestHandler);
