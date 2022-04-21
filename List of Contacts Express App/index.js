6d23cd596ddd18a9e06fd34a64a0a8121e5e3901
sa1123 <sahil2398@yahoo.com>
2022-04-22 00:58:08 +0530

    contacts 1

--- List of Contacts Express App/index.js
+++ List of Contacts Express App/index.js
@@ -5,16 +5,29 @@ const port = 8000;
 const app = express();
 
 app.set('view engine', 'ejs');
-// app.set('views', )
+app.set('views', path.join(__dirname, 'views'));
+
+var contactList = [
+    {
+        name: "A",
+        phone: "1"
+    },
+    {
+        name: "B",
+        phone: "2"
+    }
+]
 
 app.get('/', function(req, res){
     // console.log(__dirname)
     // res.send("<h1>Running</h1>");
-
-    return res.render('home', {title: "This is the title"});
+    return res.render('home', {
+        title: "Contacts list",
+        contact_list: contactList
+    });
 });
 
-app.get('/practice', function(req,res){
+app.get('/practice', function(req, res){
     return res.render('practice', {
         title: "Playing with ejs"
     });
